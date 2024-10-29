f5b7b7054d31b9c5a5dc0edc6929e2a668bd7806
Cuonq_291 <138873118+Cuonq2912@users.noreply.github.com>
2024-10-29 19:55:08 +0700

    ktra1

--- KiemTraSo1/src/Bai2.java
+++ KiemTraSo1/src/Bai2.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
-import java.lang.String;
-import java.lang.Character;
-
-
 public class Bai2 {
     public static void main(String[] args) {
-        Scanner sn = new Scanner(System.in);
-        String s = sn.nextLine();
-        String s1 = "", s2 = "";
-        for(int i = 0; i < s.length(); i++){
-            if(Character.isDigit(s.charAt(i))) {
-                s1.concat(String.valueOf(s.charAt(i)));
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                s2.append(s.charAt(i));
+            } else {
+                s1.append(s.charAt(i));
             }
-            else s2.concat(String.valueOf(s.charAt(i)));
         }
         System.out.println(s1);
         System.out.println(s2);
-    }
 
+        sc.close();
+    }
 }
