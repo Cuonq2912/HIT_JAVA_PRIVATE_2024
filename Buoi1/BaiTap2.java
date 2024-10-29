f746b9927f4ba224878bb578d744c77eef683f4e
Cuonq_291 <nguyenncuongg291@gmail.com>
2024-10-29 15:50:58 +0700

    BTVN buoi 2

--- Buoi1/BaiTap2.java
+++ Buoi1/BaiTap2.java
@@ -1,20 +1,26 @@
 package Buoi1;
+
 import java.util.Scanner;
 
 public class BaiTap2 {
+
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
-        System.out.print("Nhap so nguyen n: n = ");
         long n = sn.nextLong();
+        n = Math.abs(n);
         long ans = 1;
+        if(n == 0) {
+            System.out.print(0);
+            return;
+        }
         while(n != 0){
-            ans *= n % 10;
+            ans *= (n % 10);
             n /= 10;
         }
-        System.out.print("Tich cac chu so cua n la: " + ans);
+        System.out.println(ans);
+
     }
 }
-
 /*
 Bài 2: Viết chương trình tính tích các chữ số của một số nguyên bất kỳ.
 Ví dụ:
--- Buoi1/BaiTap3.java
+++ Buoi1/BaiTap3.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 public class BaiTap3 {
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
-        System.out.print("Nhap so luong pt: n = ");
-        int n = sn.nextInt();
-        int maxValue1 = 0, maxValue2 = 0;
-        int[] a = new int[n];
-        System.out.print("Nhap mang: ");
-        for(int i = 0; i < n; i++) {
-            a[i] = sn.nextInt();
-            if(a[i] >= maxValue1){
+//        int n = sn.nextInt();
+        long maxValue1 = 0, maxValue2 = 0;
+        Long[] a = new Long[5];
+
+        for(int i = 0; i < 5; ++i) {
+            a[i] = sn.nextLong();
+            if (a[i] >= maxValue1) {
                 maxValue2 = maxValue1;
                 maxValue1 = a[i];
+            }
+            else if(a[i] < maxValue1 && a[i] >= maxValue2){
+                maxValue2 = a[i];
             }
         }
         System.out.println("Phan tu lon nhat va lon thu 2 trong mang la: " + maxValue1 + " " + maxValue2);
@@ -27,4 +29,4 @@ Output: 10 16
 
 Input: 2 5 3 5 2
 Output: 5 5
- */
+*/
